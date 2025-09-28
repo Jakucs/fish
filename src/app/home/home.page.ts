03ee47d5ac0ede5d89f51cafed807c7a146c11c4
Jakucs Viktor <jakucs42@gmail.com>
2025-09-28 21:46:08 +0200

    we can upload images on cloudinary

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { UploadComponent } from '../upload/upload.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true,
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, CommonModule, UploadComponent],
+  providers: [HttpClient]
 })
 export class HomePage {
 
